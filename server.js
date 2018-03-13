573799ac5e8aeeab898c0255c78114c86c42213f
jaswanth12 <31659618+jaswanth12@users.noreply.github.com>
2018-03-13 23:47:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -25,6 +25,13 @@ app.get('/frd', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+counter=counter+1;
+res.send(counter.toString());
+});
+
+
 
 app.get('/developer', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-2.html'));
