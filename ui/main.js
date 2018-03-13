82be99888e2557b1685dfe922e46ded63843a878
jaswanth12 <31659618+jaswanth12@users.noreply.github.com>
2018-03-14 00:12:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var counter1=0;
 but.onclick= function()
 {
     counter1=counter1+1;
-     document.getElementyId('count').innerHTML=counter.toString();
+     document.getElementyId('count').innerHTML='hii';
     }
