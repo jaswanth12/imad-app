fe2349504f1ccd64be3efca7f8d74dbaa05a0e29
jaswanth12 <31659618+jaswanth12@users.noreply.github.com>
2018-03-17 17:46:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = requrie('pg').Pool;
+//var Pool = requrie('pg').Pool;
 
 /*var config = {
     user : 'jaswanthyenduri',
